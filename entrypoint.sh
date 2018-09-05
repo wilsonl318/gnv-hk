219fafc70b9c8cb04061d5640efc4ed3442e5e4a
fifi16 <31301257+fifi16@users.noreply.github.com>
2018-09-06 00:25:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 cd /gnv
-wget -O gnv.zip https://github.com/fifi16/gsnova-heroku/releases/download/0.33.0/gsnova_linux_amd64-latest.zip
-tar -xjvf gnv.zip
+wget -O gnv.tar https://github.com/fifi16/gsnova-heroku/releases/download/0.33.0/gsnova_linux_amd64-latest.tar
+tar -xjvf gnv.tar
 chmod +x gnv 
 ./gnv -cmd -server -listen http://:$PORT -key $KEY -window $Window -window_refresh $Window_Refresh -user "*" -log console
