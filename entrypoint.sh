e6564e296f0537a3a16f70391fdeb04042e80e53
wilson818 <32030660+wilson818@users.noreply.github.com>
2022-08-25 17:35:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 cd /gnv
-wget -O gnv.tar https://github.com/fifi16/gnv-heroku/releases/download/0.34.0/gnv_linux_amd64-0.34.0.tar
+wget -O gnv.tar https://github.com/wilson818/gnv-hk/releases/download/0.34.0/gnv_linux_amd64-0.34.0.tar
 tar -xvf gnv.tar
 chmod +x gnv 
 ./gnv -cmd -server -listen http://:$PORT -key $KEY -window $Window -window_refresh $Window_Refresh -user "*" -log console
